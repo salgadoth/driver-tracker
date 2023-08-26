f7f795dce03295480b342261f7e27f1060183f0f
Thiago <thiagobtw437@gmail.com>
2023-08-25 21:49:48 -0300

    Finished microservice

--- golang/go.mod
+++ golang/go.mod
@@ -1,3 +1,8 @@
 module github.com/salgadoth/driver-tracker/golang
 
 go 1.18
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/go-sql-driver/mysql v1.7.1
+)
---
+++ golang/internal/freight/infra/repository/route_repository_my_sql.go
@@ -0,0 +1,73 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/salgadoth/driver-tracker/golang/internal/freight/entity"
+)
+
+type RouteRepositoryMySql struct {
+	db *sql.DB
+}
+
+func NewRouteRepositoryMySql(db *sql.DB) *RouteRepositoryMySql {
+	return &RouteRepositoryMySql{
+		db: db,
+	}
+}
+
+func (r *RouteRepositoryMySql) Create(route *entity.Route) error {
+	sql := "INSERT INTO routes (id, name, distance, status, freight_price) values (?, ?, ?, ?, ?)"
+	_, err := r.db.Exec(sql, route.ID, route.Name, route.Distance, route.Status, route.FreightPrice)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *RouteRepositoryMySql) FindById(id string) (*entity.Route, error) {
+	sqlSmt := "SELECT id, name, distance, status, freight_price, started_at, finished_at FROM routes WHERE id = ?"
+	row := r.db.QueryRow(sqlSmt, id)
+
+	var startedAt, finishedAt sql.NullTime
+
+	var route entity.Route
+
+	err := row.Scan(
+		&route.ID,
+		&route.Name,
+		&route.Distance,
+		&route.Status,
+		&route.FreightPrice,
+		&startedAt,
+		&finishedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if startedAt.Valid {
+		route.StartedAt = startedAt.Time
+	}
+
+	if finishedAt.Valid {
+		route.FinishedAt = finishedAt.Time
+	}
+
+	return &route, nil
+}
+
+func (r *RouteRepositoryMySql) Update(route *entity.Route) error {
+	startedAt := route.StartedAt.Format("2006-01-02 15:04:03")
+	finishedAt := route.FinishedAt.Format("2006-01-02 15:04:03")
+	sql := "UPDATE routes SET status = ?, freight_price = ?, started_at=?, finished_at = ? WHERE id = ?"
+
+	_, err := r.db.Exec(sql, route.Status, route.FreightPrice, startedAt, finishedAt, route.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
